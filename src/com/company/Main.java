6a7d5dc3a0e489d5340ea071323bf55afb9f3ec7
Mohsin <msis.rubel@gmail.com>
2020-06-12 16:36:12 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+
+
+
+
+
+/* thread is built-in support for multi-thread programming, A multi-thread program contains two or
+more parts that can run concurrently, each part of such program called thread and each thread defines
+a separate path of execution. when java programs starts up, one thread begins running immediately that's
+ called main thread, because this thread executed when program begins. the main thread is created
+  automatically.
+  # thread is light-weight sub process
+  # thread follow inherited rules like super thread & child threads,child inherits parent.
+  # main thread can call currentThread();
+  # It is the smallest independent unit of a program
+  # Every Java programs contains at least one thread
+  # A thread is created & controlled by the Java.lang.Thread Class
+ */
