0cdda39cfa0d8551d3ec2da044223b76facb6612
Mohsin <msis.rubel@gmail.com>
2020-06-12 23:32:50 -0400

    4,5,6,7 multiplication added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,10 +10,32 @@ public class Main {
         // thread is a execution context
 
         System.out.println("==Application Started=="); //job 1
-
-        for (int doc=1; doc<=10;doc++){
-            System.out.println("printing document #" + doc); // job 2
+        //creating multiplication tables 4
+        int num1 = 4;
+        for( int i =1; i<= 10; i++)// first initialize then conditions provided then value incremented
+        {
+            System.out.println(num1+ "x" + i + "=" + num1*i );// Concatenation
+            // if i want this value will provide by user then need to use Scanner class
         }
+        int num2 = 5;
+        for( int i =1; i<= 10; i++) {
+            System.out.println(num2+ "x" + i + "=" + num2*i );
+        }
+        int num3 = 6;
+        for( int i =1; i<= 10; i++) {
+            System.out.println(num3+ "x" + i + "=" + num3*i );
+        }
+        int num4 = 7;
+        for( int i =1; i<= 10; i++) {
+            System.out.println(num4+ "x" + i + "=" + num4*i );
+        }
+
+
+
+
+       /*for (int doc=1; doc<=10;doc++){
+           System.out.println("printing document #" + doc); // job 2
+        } */
 
         System.out.println("== Application Finished=="); //job 3
 
@@ -26,11 +48,7 @@ public class Main {
 
 
 
-/* thread is built-in support for multi-thread programming, A multi-thread program contains two or
-more parts that can run concurrently, each part of such program called thread and each thread defines
-a separate path of execution. when java programs starts up, one thread begins running immediately that's
-called main thread, because this thread executed when program begins. the main thread is created
-  automatically.
+/*
   # thread is light-weight sub process
   # thread follow inherited rules like super thread & child threads,child inherits parent.
   # main thread can call currentThread();
@@ -41,4 +59,17 @@ called main thread, because this thread executed when program begins. the main t
   # playing song or downloading image can be good example of thread
   # We can create thread by implementing runnable interface or extends thread
   # What make Java applications concurrent? java.lang.Thread/ java.lang.Runnable/ java.util.concurrent
+  # only one thread run at a time in any given process.
+  # if we want to inherit & override, its good to use extending thread but if we just want to
+ creating thread & we do not intend inherit class then use runnable interface, its lot more flexible.
+ we cannot inherit multiple class in java
+  # thread-scheduler decide which thread to run, each thread has a priority, High priority tasks
+ executed until higher priority tasks appear
+ # multi-thread is the ability of the program to run two or more threads concurrently,
+ where each thread can handle a different task at the same time making optimal use of the available resources.
  */
+
+
+
+
+
