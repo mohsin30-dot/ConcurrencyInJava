0c09792549cba13bab28cf50e7eca51d35900848
Mohsin <msis.rubel@gmail.com>
2020-06-14 13:10:11 -0400

    -Runnable objects creation using class -Runnable using Anonymous class - Thread using runnable - sleep method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,8 @@
 package com.company;
 
-public class Main {
+class ThreadDemo {
     // main method represents main thread
+    // class cannot declare in any method
 
     public static void main(String[] args) {
         // write your code here
@@ -9,36 +10,82 @@ public class Main {
         // threads always execute the jobs in a sequence
         // thread is a execution context
 
-        System.out.println("==Application Started=="); //job 1
+        System.out.println("==Application Started=="); //thread started
         //creating multiplication tables 4
-        int num1 = 4;
+
+        RunnableDemo1 runnableDemo1 = new RunnableDemo1(4);
+        Thread thread1 = new Thread(runnableDemo1); // constructor overloading
+        thread1.start();// after start method call thread will start
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) { // exception handling
+            e.printStackTrace();
+        }
+
+        RunnableDemo1 runnableDemo2 = new RunnableDemo1(5);
+        Thread thread2 = new Thread(runnableDemo2); // constructor overloading
+        thread2.start();// after start method call thread will start
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) { // exception handling
+            e.printStackTrace();
+        }
+
+
+        RunnableDemo1 runnableDemo3 = new RunnableDemo1(6);
+        Thread thread3 = new Thread(runnableDemo3); // constructor overloading
+        thread3.start();// after start method call thread will start
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) { // exception handling
+            e.printStackTrace();
+        }
+
+        RunnableDemo1 runnableDemo4 = new RunnableDemo1(7);
+        Thread thread4 = new Thread(runnableDemo4); // constructor overloading
+        thread4.start();// after start method call thread will start
+
+
+        /*Thread thread2 = */
+       /* new Thread(new Runnable(){ // anonymous class created
+            @Override
+            public void run() {
+                int num1 = 5;
+                for( int i =1; i<= 10; i++)// first initialize then conditions provided then value incremented
+                {
+                    System.out.println(num1+ "x" + i + "=" + num1*i );// Concatenation
+                    // if i want this value will provide by user then need to use Scanner(import java.util.Scanner) class
+                }
+            }
+        }).start(); // method chaining
+        //thread2.start();
+
+
+       /* for (int doc=1; doc<=10;doc++){
+           System.out.println("printing document #" + doc); //
+        }
+        */
+
+        System.out.println("== Application Finished=="); //thread completed
+
+
+    }
+}
+
+
+class RunnableDemo1 implements Runnable {
+    int multiplicationNumber;
+    RunnableDemo1(int multiplicationNumber){
+        this.multiplicationNumber = multiplicationNumber;
+    }
+    @Override
+    public void run() {
+        int num1 = multiplicationNumber; // dynamic way to multiply
         for( int i =1; i<= 10; i++)// first initialize then conditions provided then value incremented
         {
             System.out.println(num1+ "x" + i + "=" + num1*i );// Concatenation
-            // if i want this value will provide by user then need to use Scanner class
+            // if i want this value will provide by user then need to use Scanner(import java.util.Scanner) class
         }
-        int num2 = 5;
-        for( int i =1; i<= 10; i++) {
-            System.out.println(num2+ "x" + i + "=" + num2*i );
-        }
-        int num3 = 6;
-        for( int i =1; i<= 10; i++) {
-            System.out.println(num3+ "x" + i + "=" + num3*i );
-        }
-        int num4 = 7;
-        for( int i =1; i<= 10; i++) {
-            System.out.println(num4+ "x" + i + "=" + num4*i );
-        }
-
-
-
-
-       /*for (int doc=1; doc<=10;doc++){
-           System.out.println("printing document #" + doc); // job 2
-        } */
-
-        System.out.println("== Application Finished=="); //job 3
-
 
     }
 }
